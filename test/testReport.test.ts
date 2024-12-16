12d1f6d963de9dee173665244e193e3e9398a00c
Ritsaert Hornstra <ritsaert@continuit.nl>
2024-12-16 22:58:27 +0100

    Updated tests

--- test/testReport.test.ts
+++ test/testReport.test.ts
@@ -1,7 +1,17 @@
 import { createTestReport } from '../src/testReport.ts';
 
-Deno.test('testReport_deno', async () => {
-  const testReport = await createTestReport('./test_data/deno/testreport.json');
+Deno.test('testReport_deno_success', async () => {
+  const testReport = await createTestReport('./test_data/deno_success/testreport.json');
+  console.log(testReport);
+});
+
+Deno.test('testReport_deno_failed', async () => {
+  const testReport = await createTestReport('./test_data/deno_failed/testreport.json');
+  console.log(testReport);
+});
+
+Deno.test('testReport_deno_disabled', async () => {
+  const testReport = await createTestReport('./test_data/deno_disabled/testreport.json');
   console.log(testReport);
 });
 
